1a942f81b4d74bf004987474e4cf640390f5679b
Davi Rodrigues <developer.davi@gmail.com>
2022-10-21 13:36:46 -0300

    fix: change package name to @devdavi/unflatten

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,9 @@
 
 const yargs = require('yargs');
 
-const usage = '\nUsage: solidity-unflattener <file_name> [<output_dir>] [--wd]';
+const usage =
+  '\nUsage: npx @devdavi/unflatten <file_name> [<output_dir>] [--wd]';
+
 yargs
   .usage(usage)
   .option('wd', {
